78db5fc377a663048749cd1f4da5f4138073d1aa
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-08-24 12:44:30 +0530

    fixed lint errors

--- src/lib.js
+++ src/lib.js
@@ -1,24 +1,49 @@
 "use strict";
-const extractDigits = function(number){
-  const onesPlace = number%10;
+const extractDigits = function(number) {
+  const onesPlace = number % 10;
   const firstNum = number - onesPlace;
-  return [firstNum, onesPlace]
+  return [firstNum, onesPlace];
 };
 
 const getNumberInWords = function(number) {
+  let numberNameList = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "10": "Ten",
+    "11": "Eleven",
+    "12": "Twelve",
+    "13": "Thirteen",
+    "14": "Fourteen",
+    "15": "Fifteen",
+    "16": "Sixteen",
+    "17": "Seventeen",
+    "18": "Eighteen",
+    "19": "Nineteen",
+    "20": "Twenty",
+    "30": "Thirty",
+    "40": "Fourty",
+    "50": "Fifty",
+    "60": "Sixty",
+    "70": "Seventy",
+    "80": "Eighty",
+    "90": "Ninty",
+    "100": "Hundred"
+  };
 
-  let numberNameList = {'1':'One','2':'Two','3':'Three','4':'Four','5':'Five','6':'Six','7':'Seven','8':'Eight','9':'Nine',
-    '10':'Ten','11':'Eleven','12':'Twelve','13':'Thirteen','14':'Fourteen','15':'Fifteen','16':'Sixteen','17':'Seventeen',
-    '18':'Eighteen','19':'Nineteen','20':'Twenty','30':'Thirty','40':'Fourty','50':'Fifty','60':'Sixty','70':'Seventy',
-    '80':'Eighty','90':'Ninty','100':'Hundred'};
-
-  if(Object.keys(numberNameList).includes(number)){
+  if (Object.keys(numberNameList).includes(number)) {
     return numberNameList[number];
   }
   let splittedNumbers = extractDigits(number);
   let firstPart = splittedNumbers[0];
   let secondPart = splittedNumbers[1];
-  return (numberNameList[firstPart] + numberNameList[secondPart]);
+  return numberNameList[firstPart] + numberNameList[secondPart];
 };
 
 exports.extractDigits = extractDigits;
